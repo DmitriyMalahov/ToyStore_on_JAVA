00b613d409a632e0aa5565da7fede54f74b103a8
Dmitriy Malakhov <lesnichiy1986@gmail.com>
2023-10-26 10:52:08 +0300

    finished Main

--- ToyStore/Main.java
+++ ToyStore/Main.java
@@ -4,5 +4,38 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
-
+    public static void main(String[] args) {
+        ToyStore toyStore = new ToyStore();
+    
+        toyStore.addToy(new Toy(1, "Машинка", 0.1));
+        toyStore.addToy(new Toy(2, "Поезд", 0.3));
+        toyStore.addToy(new Toy(3, "Меч", 0.4));
+        toyStore.addToy(new Toy(4, "Слон", 0.2));
+        toyStore.addToy(new Toy(5, "Самосвал", 0.3));
+        toyStore.addToy(new Toy(6, "Мяч", 0.4));
+        toyStore.addToy(new Toy(7, "Обезьянка", 0.2));
+        toyStore.addToy(new Toy(8, "Кубик", 0.3));
+        toyStore.addToy(new Toy(9, "Динозавр", 0.2));
+        toyStore.addToy(new Toy(10, "Кукла", 0.4));
+    
+        try {
+          FileWriter writer = new FileWriter("ToyStore\\result.txt");
+    
+          int counter = 0;
+          int maxToys = 10;
+          while (counter < maxToys) {
+            Toy toy = toyStore.getRandomToy();
+    
+            if (toy != null) {
+              counter++;
+              writer.write(counter + ". " + "Выбрана игрушка: " + toy.getName()
+               + " (Шанс выпадения" + " " + toy.getChance() +  "%" + ")\n");
+            }
+          }
+    
+          writer.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
 }
